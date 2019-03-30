b76b1be30a3156b9394e3ef39400d73337987d66
Alice Le <lealice@gmail.com>
2019-03-29 18:07:11 -0700

    finalized product list added error handling to endpoint

--- public/api/getproducts.php
+++ public/api/getproducts.php
@@ -1,35 +1,41 @@
 <?php
 
-// phpinfo();
 require_once('mysqlconnect.php');
+require_once('functions.php');
+
+set_exception_handler('handleError');
 
 $query = 'SELECT p.`id`, p.`name`, p.`price`,
-i.`url` AS `images`
+    i.`url` AS `images`
 FROM `products` AS p
 JOIN `images` AS i
-ON p.`id` = i.`products_id`';
+    ON p.`id` = i.`products_id`
+ORDER BY p.`id`';
 
 /*procedural*/
 $result = mysqli_query($conn, $query);
 
-$data = [];
-$images = [];
+if(!result){
+    throw new Exception('invalid query: '. mysqli_error($conn));
+}
 
-while($row = mysqli_fetch_assoc($result)){
+$data = [];
+// $images = [];
+
+while($row = mysqli_fetch_assoc($result)){ 
     $currentID = $row['id'];
+    $currentID = intval($currentID);
     $image = $row['images'];
     if(isset($data[$currentID]) ){
-        $data[$row['id']] ['images'] [] = $row['images'];
+        $data[$row['id']] ['images'][] = $row['images'];
         $data[$currentID]['images'][]= $image; //new push way
         // array_push($data[$currentID] ['images'], $image ); //old push way
         // $data[$currentID]['images'][count($data[$currentID]['images'])] = $image //this is just mean
     } else {
-        $image = $row['images'];
-        $row['images'] = [$image];
-        //the way i would write
+        unset($row['images']);
+        $row['images'] = []; //sub array
+        $row['images'][] = $image;
         $row['price'] = intval($row['price']);
-        $row['price'] = (int)$row['price'];
-
         $data[$currentID] = $row;
         
     }
