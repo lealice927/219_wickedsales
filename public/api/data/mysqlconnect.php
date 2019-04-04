da7815a813413d15e67a337a4e40d47dddec86cb
Alice Le <lealice@gmail.com>
2019-04-04 14:13:16 -0700

    added product details endpoint and updated sql dump connected to database tested incoming data made query for product details altered outbound data json encdoed handled error

--- public/api/data/mysqlconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*procedural*/
-$conn = mysqli_connect('localhost', 'root', '', 'wickedsales', 3306);
-
-/*oop style*/
-$db = new mysqli('localhost', 'root', '', 'wickedsales', 3306);
-?>
--- public/api/functions.php
+++ public/api/functions.php
@@ -4,7 +4,7 @@ if(!function_exists('handleError')){
     function handleError($error){
         $output = [
             'success' => false,
-            'error' => $error -> getMessage();
+            'error' => $error -> getMessage()
         ];
         $json_output = json_encode($output);
         print($json_output);
