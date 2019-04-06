6018dbea0696e93801b71dfd00cf71c86433ca08
Alice Le <lealice@gmail.com>
2019-04-05 18:06:23 -0700

    Completed client view product details

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import ProductList from './product_list';
 import ProductDetails from './product_details';
+import './products.scss';
 
 export default props => {
 
     return (
-        <div>
-            <Route path="/products" exact component={ProductList}/>
-            <Route path="/products/:product_id" component={ProductDetails}/>
+        <div className="products">
+            <Route path="/products" exact component={ProductList} />
+            <Route path="/products/:product_id" component={ProductDetails} />
         </div>
     );
 }
--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ProductDetails extends Component {
-    componentDidMount() {
+    state = {
+        details: null
+    }
+
+    async componentDidMount() {
         const { params } = this.props.match;
         //Call server to get product details
 
-        console.log('Fetch product with id of:', params.product_id);
+        // console.log('Fetch product with id of:', params.product_id);
+
+        const resp = await axios.get(`/api/getproductdetails.php?productId=${params.product_id}`);
+        console.log('Resp:', resp);
+
+        if (resp.data.success) {
+            this.setState({
+                details: resp.data.productInfo
+            });
+        } else {
+            this.setState({
+                details: false
+            });
+        }
+
+        this.setState({
+            details: resp.data.productInfo
+        });
     }
     render() {
+        console.log('Product Details:', this.state.details);
+        const { details } = this.state;
+
+        if (!details === null) {
+            return <h1>Loading...</h1>
+        } else if (!details) {
+            return <h1 className="center">No Product Found</h1>
+        }
+
+        const { description = 'No description avaialable', name } = details;
 
         return (
             <div className="product-details">
-                <h1 className="center">[Product Name] Details</h1>
+                <h1 className="center">{name}</h1>
+                <p>{description}</p>
             </div>
         );
     }
