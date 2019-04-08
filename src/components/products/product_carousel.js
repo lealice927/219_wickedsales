5231b740708cb1bf94ef3fb9cdb3e25fe5ed4980
Alice Le <lealice@gmail.com>
2019-04-08 16:10:04 -0700

    added buttons and misc details

--- src/components/products/product_carousel.js
+++ src/components/products/product_carousel.js
@@ -2,24 +2,29 @@ import React, {Component} from 'react';
 
 class ProductCarousel extends Component {
     componentDidMount(){
-        console.log('Carousel Div:', this.carousel);
+        // console.log('Carousel Div:', this.carousel);
 
-        M.Carousel.init(this.carousel);
+        const config={
+            numVisible: 3,
+            indicators: true
+        };
+
+        M.Carousel.init(this.carousel, config);
     }
 
     render(){
-        console.log('Props:', this.props);
+        // console.log('Props:', this.props);
 
         const items = this.props.images.map((img) => {
             return(
-                <a className="carousel-item" href="#">
-                    <img key={img} src={`/dist/${img}`} alt="Product Image" />
+                <a key={img} className="carousel-item" href="#">
+                    <img src={`/dist/${img}`} alt="Product Image" />
                 </a>
             );
         });
 
         return(
-            <div ref={(element) => this.carousel = element} className="carousel">
+            <div ref={(element) => this.carousel = element} className="col s12 m8 carousel">
                 {items}
             </div>
         );
--- src/components/products/product_details.js
+++ src/components/products/product_details.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import ProductCarouself from './product_carousel';
 import ProductCarousel from './product_carousel';
+import MiscDetails from './misc_details';
+import { formatMoney } from '../../helpers';
 
 class ProductDetails extends Component {
     state = {
@@ -15,7 +16,7 @@ class ProductDetails extends Component {
         // console.log('Fetch product with id of:', params.product_id);
 
         const resp = await axios.get(`/api/getproductdetails.php?productId=${params.product_id}`);
-        console.log('Resp:', resp);
+        // console.log('Resp:', resp);
 
         if (resp.data.success) {
             this.setState({
@@ -32,7 +33,7 @@ class ProductDetails extends Component {
         });
     }
     render() {
-        console.log('Product Details:', this.state.details);
+        // console.log('Product Details:', this.state.details);
         const { details } = this.state;
 
         if (!details === null) {
@@ -41,8 +42,8 @@ class ProductDetails extends Component {
             return <h1 className="center">No Product Found</h1>
         }
 
-        const { description = 'No description available', name, images } = details;
-        
+        const { description = 'No description available', name, images, price, miscDetails } = details;
+
 
         //================ MY WAY OF SHOWING ALL IMAGES IN THE ARRAY=========================//
         // const imageList = images.map( (image, index) =>{
@@ -54,8 +55,29 @@ class ProductDetails extends Component {
         return (
             <div className="product-details">
                 <h1 className="center">{name}</h1>
-                <ProductCarousel images={images} />
-                <p>{description}</p>
+                <div className="row">
+                    <ProductCarousel images={images} />
+                    <div className="col s12 m4">
+                        <div className="right-align product-price">{formatMoney(price)}</div>
+                        <div className="right-align add-to-cart">
+                            <span className="qty-container">
+                                <button className="btn btn-small btn-floating purple lighten-1">
+                                    <i className="material-icons">remove</i>
+                                </button>
+                                <span className="product-qty">1</span>
+                                <button className="btn btn-small btn-floating purple lighten-1">
+                                    <i className="material-icons">add</i>
+                                </button>
+                            </span>
+
+                            <button className="btn purple darken-2">
+                                <i className="material-icons">add_shopping_cart</i>
+                            </button>
+                        </div>
+                        <p>{description}</p>
+                        <MiscDetails details={miscDetails} />
+                    </div>
+                </div>
                 {/* <div>
                     {imageList}
                 </div> */}
