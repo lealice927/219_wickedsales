467f7865b99dde399d54862af04c97c82b18cf7b
Alice Le <lealice@gmail.com>
2019-04-16 17:46:30 -0700

    Completed sign in and sign up form

--- src/components/account/sign_up/sign_up_form.js
+++ src/components/account/sign_up/sign_up_form.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import Input from '../../general/input';
 
-const SignInForm = props => {
+const SignUpForm = props => {
     // console.log('Sign In Form Props:', props);
-    const { handleSubmit, signIn } = props;
+    const { handleSubmit, signUp } = props;
 
     return (
-        <form onSubmit={handleSubmit(signIn)}>
+        <form onSubmit={handleSubmit(signUp)}>
             {/*if we want to test what parameters it is given, we can CL it like this */}
             <div className="row">
+                <Field col="s12" id="name" name="name" component={Input} label="Name" />
                 <Field col="s12" id="email" name="email" component={Input} label="Email" />
                 <Field col="s12" id="password" name="password" component={Input} type="password" label="Password" />
+                <Field col="s12" id="confirmpassword" name="confirmpassword" component={Input} type="password" label="Confirm Password" />
+
             </div>
 
             <div className="row">
                 <div className="col s12 right-align">
-                    <button className="btn purple darken-2">Sign In</button>
+                    <button className="btn purple darken-2">Sign Up</button>
                 </div>
             </div>
         </form>
     )
 }
 
-function validate({email, password}) {
+function validate({name, email, password, confirmpassword}) {
     const errors = {};
+
+    if(!name) {
+        errors.name = 'Please enter full name';
+    }
 
     if (!email) {
         errors.email = 'Please enter your email';
@@ -34,10 +41,14 @@ function validate({email, password}) {
         errors.password = 'Please enter your password';
     }
 
+    if(!confirmpassword) {
+        errors.confirmpassword = 'Password does not match';
+    }
+
     return errors;
 }
 
 export default reduxForm({
-    form: 'sign-in-form', //this is us setting the name of the form, THIS IS MADE-UP!!!! 
+    form: 'sign-up-form', //this is us setting the name of the form, THIS IS MADE-UP!!!! 
     validate: validate
-})(SignInForm);
+})(SignUpForm);
--- src/components/general/input.js
+++ src/components/general/input.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
-export default props => {
-    console.log('Input Props:', props);
-
-    const { input, id, label, type = 'text', col='s12' } = props;
+export default ({ input, id, label, meta: { error, touched }, type = 'text', col = 's12' }) => {
 
     return (
-        // <div className="input-field">
         <div className={`input-field col ${col}`}>
-            <input {...input} id={id} type={type} /> 
+            <input {...input} id={id} type={type} />
             <label htmlFor={id}>{label}</label>
+            <p className="red-text text-darken-2">{touched && error}</p>
         </div>
     );
 }
